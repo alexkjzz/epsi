7eb167cb57c47642d67bfdab9df7d1ccf3632fd3
Etienne Divina <etienne.divina@gmail.com>
2023-03-19 08:41:13 +0100

    fizzbuzz from scratch

---
+++ FizzBuzzFromScratch/FizzBuzzTests/FizzBuzzHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass()]
+    public class FizzBuzzHelper_GetFizzBuzzShould
+    {
+        [TestMethod]
+        public void ReturnX_WhenX()
+        {
+            //Given --> initialisation de l'état
+            int input = X;
+
+            //When --> Action
+            string actual = FizzBuzzHelper.GetFizzBuzz(input);
+
+            //Then --> assertion
+            Assert.AreEqual("X", actual);
+        }
+
+    }
+}
