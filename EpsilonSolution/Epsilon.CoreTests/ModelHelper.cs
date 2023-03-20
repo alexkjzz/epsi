5756fda89520954457bb8270bfa07492781ab8e7
Etienne Divina <etienne.divina@gmail.com>
2023-03-20 22:31:43 +0100

    epsilon

---
+++ EpsilonSolution/Epsilon.CoreTests/ModelHelper.cs
@@ -0,0 +1,17 @@
+﻿using Epsilon.Core.Model;
+
+namespace Epsilon.CoreTests
+{
+    internal static class ModelHelper
+    {
+        public static Eleve CreateEleve(string nom, params decimal[] notes)
+        {
+            var eleve = new Eleve()
+            {
+                Nom = nom
+            };
+            eleve.Notes.AddRange(notes);
+            return eleve;
+        }
+    }
+}
